d53403751be351b6297cecdf9f89b111870975d1
Brandon Perfetti <brandon@brandonperfetti.com>
2024-04-20 11:51:35 -0700

    ✨ (page.tsx): Add ArticleCard component to display individual articles in a grid layout The ArticleCard component is imported and used within the ArticlesIndex function to display individual articles in a grid layout. This change enhances the user experience by presenting articles in a visually appealing manner, improving readability and engagement on the page.

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,3 +1,4 @@
+import { ArticleCard } from '@/components/ArticleCard'
 import { FilterableArticles } from '@/components/FilterableArticles'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { getAllArticles } from '@/lib/articles'
@@ -18,9 +19,13 @@ export default async function ArticlesIndex() {
       title="Writing on mindset, software design, company building, and the tech industry."
       intro="All of my long-form thoughts on mindset, programming, leadership, product design, and more, collected in chronological order."
     >
-      <Suspense fallback={<div>Loading...</div>}>
-      <FilterableArticles articles={articles} />
-      </Suspense>
+      <div>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {articles.map((article) => (
+            <ArticleCard key={article.slug} article={article} />
+          ))}
+        </div>
+      </div>
     </SimpleLayout>
   )
 }
