110217e53c2b5ac189c51e258366d31b66570c61
brandonperfetti <brandon@brandonperfetti.com>
2026-02-20 17:29:24 -0800

    Adds site description constant

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,4 +1,6 @@
 const DEFAULT_SITE_URL = 'https://brandonperfetti.com'
+export const SITE_DESCRIPTION =
+  'I’m Brandon, a product and project manager plus software engineer based in Orange County, California.'
 
 export function getSiteUrl() {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? DEFAULT_SITE_URL
